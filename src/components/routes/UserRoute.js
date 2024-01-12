f4e8d048706467003c19adfa894af1050c29e77c
priydarshi12 <21001016054.jcboseust.ac.in>
2024-01-12 22:00:48 +0530

    redirect countdown

--- src/components/routes/UserRoute.js
+++ src/components/routes/UserRoute.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Route, Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-
+import  LoadingToRedirect  from "./LoadingToRedirect";
 const UserRoute = ({ children, ...rest }) => {
   const { user } = useSelector((state) => ({ ...state }));
 
   return user && user.token ? (
     <Route {...rest} render={() => children}></Route>
   ) : (
-    <h1>Loading </h1>
+    <LoadingToRedirect />
   );
 };
 
