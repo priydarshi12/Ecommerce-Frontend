e4c654cba3e3604d57bc923f398a0fbd95242e22
priydarshi12 <21001016054.jcboseust.ac.in>
2024-01-12 21:45:00 +0530

    protecting user route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -6,74 +6,72 @@ import "react-toastify/dist/ReactToastify.css";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Home from "./pages/Home";
+import History from "./pages/user/History";
 import Header from "./components/nav/Header";
+import UserRoute from "./components/routes/UserRoute";
 import RegisterComplete from "./pages/auth/RegisterComplete";
 
 import { auth } from "./firebase";
-import {useDispatch} from 'react-redux'
+import { useDispatch } from "react-redux";
 import { toast } from "react-toastify";
 
 import { currentUser } from "./functons/auth";
 function App() {
-  const dispatch=useDispatch();
-  useEffect(()=>{
-    const unsubscribe=auth.onAuthStateChanged(async(user)=>{
-      if(user){
-        console.log(user)
-        const idTokenResult=await user.getIdTokenResult();
-        // dispatch({
-        //   type:"LOGGED_IN_USER",
-        //   payload:{
-        //     email:user.email,
-        //     token:idTokenResult.token,
-        //   },
-        // });
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        console.log(user);
+        const idTokenResult = await user.getIdTokenResult();
+       
         currentUser(idTokenResult.token)
-        .then((res) => {
-          console.log("response from the backend on login",res);
-          if(res.status===201){
-          console.log("response from backend on login ",res);
-          const { displayName } = user.reloadUserInfo;
-          console.log("google result token->", displayName);
-          dispatch({
-            type: "CURRENT_USER",
-            payload: {
-              email: user.email,
-              name: displayName,
-              token: idTokenResult.token,
-              role:res.data.role,
-              _id:res.data._id,
-            },
+          .then((res) => {
+            console.log("response from the backend on login", res);
+            if (res.status === 201) {
+              console.log("response from backend on login ", res);
+              const { displayName } = user.reloadUserInfo;
+              console.log("google result token->", displayName);
+              dispatch({
+                type: "CURRENT_USER",
+                payload: {
+                  email: user.email,
+                  name: displayName,
+                  token: idTokenResult.token,
+                  role: res.data.role,
+                  _id: res.data._id,
+                },
+              });
+            }
+          })
+          .catch((err) => {
+            console.log("user not found->", err.message);
+            if (err.response && err.response.status === 401) {
+              toast.error("Please register yourself");
+              dispatch({
+                type: "USER NOT FOUND",
+                payload: null,
+              });
+            }
           });
-    
-         
-        }
-      })
-        .catch((err) =>{
-          console.log("user not found->",err.message)
-          if(err.response && err.response.status === 401){
-          toast.error("Please register yourself")
-          dispatch({
-            type: "USER NOT FOUND",
-            payload: null,
-          });
-          }
-        });
       }
     });
     //cleanup
-    return ()=>unsubscribe()
-  },[]);
+    return () => unsubscribe();
+  }, []);
   return (
     <>
       <Header />
-      <ToastContainer/>
+      <ToastContainer />
       <Switch>
         <Route exact path="/" component={Home}></Route>
         <Route exact path="/login" component={Login}></Route>
         <Route exact path="/register" component={Register}></Route>
-        <Route exact path="/register/complete" component={RegisterComplete}></Route>
-
+        <Route
+          exact
+          path="/register/complete"
+          component={RegisterComplete}
+        ></Route>
+        <UserRoute exact path="/user/history" component={History}></UserRoute>
       </Switch>
     </>
   );
---
+++ src/components/routes/UserRoute.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const UserRoute = ({ children, ...rest }) => {
+  const { user } = useSelector((state) => ({ ...state }));
+
+  return user && user.token ? (
+    <Route {...rest} render={() => children}></Route>
+  ) : (
+    <h1>Loading </h1>
+  );
+};
+
+export default UserRoute;
