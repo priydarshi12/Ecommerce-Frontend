ffd0e3dc8f8f279cf30bf7ab964025c7ec87fc22
priydarshi12 <21001016054.jcboseust.ac.in>
2024-01-16 17:51:21 +0530

    category rander and delete

--- src/pages/admin/category/CategoryCreate.js
+++ src/pages/admin/category/CategoryCreate.js
@@ -7,18 +7,20 @@ import {
   getCategories,
   removeCategory,
 } from "../../../functons/category";
-
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 const CategoryCreate = () => {
   const { user } = useSelector((state) => ({ ...state }));
   const [name, setName] = useState("");
   const [loading, setLoading] = useState(false);
   const [categories, setCategories] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     loadCategories();
-  },[])
+  }, []);
 
-  const loadCategories=()=>getCategories().then(c=>setCategories(c.data))
+  const loadCategories = () =>
+    getCategories().then((c) => setCategories(c.data));
   const handleSubmit = (e) => {
     e.preventDefault();
     setLoading(true);
@@ -27,12 +29,31 @@ const CategoryCreate = () => {
         setLoading(false);
         setName("");
         toast.success(`"${res.data.name}" is created`);
+        loadCategories();
       })
       .catch((err) => {
         setLoading(false);
         setName("");
         if (err.resonse.status === 400) toast.error(err.response.data);
       });
+  };
+  const handleRemove = async (slug) => {
+    if (window.confirm("Delete")) {
+      setLoading(true);
+      removeCategory(slug, user.token)
+        .then((res) => {
+          console.log(res);
+          setLoading(false);
+          toast.error(`${res.data.name} deleted`)
+          loadCategories();
+        })
+        .catch((err) => {
+          if (err.response.status === 400) {
+            setLoading(false);
+            toast.error(err.response.data);
+          }
+        });
+    }
   };
   const categoryForm = () => {
     return (
@@ -68,7 +89,22 @@ const CategoryCreate = () => {
           )}
           {categoryForm()}
           <hr />
-          {categories.length}
+          {categories.map((c) => (
+            <div className="alert alert-secondary" key={c.id}>
+              {c.name}{" "}
+              <span
+                onClick={() => handleRemove(c.slug)}
+                className="btn btn-sm float-right"
+              >
+                <DeleteOutlined className="text-danger" />
+              </span>
+              <Link to={`/admin/category/${c.slug}`}>
+                <span className="btn btn-sm float-right">
+                  <EditOutlined className="text-warning" />
+                </span>
+              </Link>
+            </div>
+          ))}
         </div>
       </div>
     </div>
