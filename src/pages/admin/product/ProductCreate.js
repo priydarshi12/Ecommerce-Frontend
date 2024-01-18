660dc0f8e87c32fd598f41c17a0e6fea16dbc3c9
priydarshi12 <21001016054.jcboseust.ac.in>
2024-01-18 20:15:52 +0530

    create product error message

--- src/pages/admin/product/ProductCreate.js
+++ src/pages/admin/product/ProductCreate.js
@@ -52,7 +52,7 @@ const ProductCreate = () => {
       })
       .catch((err) => {
         console.log(err);
-        if (err.response.status === 400) toast.error(err.response.data);
+        if (err.response.status === 400) toast.error(err.response.data.err);
       });
     
   };
