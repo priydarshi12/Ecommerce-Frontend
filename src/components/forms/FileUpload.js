423d1a9d07d2ad48451535e0b7faa86e71c11eb7
priydarshi12 <21001016054.jcboseust.ac.in>
2024-02-02 20:17:42 +0530

    file resize in react

--- src/components/forms/FileUpload.js
+++ src/components/forms/FileUpload.js
@@ -1,10 +1,30 @@
 import React from "react";
-
+import Resizer from "react-image-file-resizer"
+import axios from "axios"
+import { useSelector } from "react-redux";
 const FileUpload = () => {
 
-
-    const fileUploadAndResize=()=>{
+    const {user}=useSelector((state)=>({...state}))
+    const fileUploadAndResize=(e)=>{
        //resize
+        let files=e.target.files;
+        console.log("fileupload",files);
+        if(files){
+            for(let i=0;i<files.length;i++){
+                Resizer.imageFileResizer(
+                    files[i],
+                    720,
+                    720,
+                    "JPEG",
+                    100,
+                    0,
+                    (uri) => {
+                      console.log(uri);
+                    },
+                    "base64"
+                  );
+            }
+        }
        //send back to server to upload to clodinary
        //set url to images[] in the parent components -ProductCreate 
     }
