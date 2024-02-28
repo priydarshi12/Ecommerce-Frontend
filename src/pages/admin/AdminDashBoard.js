9f27f4475b084d17391995695552770e71ba8054
priydarshi12 <21001016054.jcboseust.ac.in>
2024-02-28 10:53:29 +0530

    admin products card

--- src/pages/admin/AdminDashBoard.js
+++ src/pages/admin/AdminDashBoard.js
@@ -1,13 +1,48 @@
 import React from "react";
+import  { useEffect, useState } from "react";
 import AdminNav from "../../components/nav/AdminNav";
+import { getProductsByCount } from "../../functons/product";
+import AdminProductCard from "../../components/cards/AdminProductCard";
 const AdminDashBoard = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    loadAllProducts();
+  }, []);
+
+  const loadAllProducts = () => {
+    setLoading(true);
+    getProductsByCount(100)
+      .then((res) => {
+        setProducts(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log(err);
+      });
+  };
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-md-2">
           <AdminNav />
         </div>
-        <div className="col">Admin page</div>
+
+        <div className="col">
+          {loading ? (
+            <h4 className="text-danger">Loading...</h4>
+          ) : (
+            <h4>All Products</h4>
+          )}
+          <div className="row">
+            {products.map((product) => (
+              <div key={product._id} className="col-md-4">
+                <AdminProductCard product={product} />
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
