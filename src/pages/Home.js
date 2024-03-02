5e228ce3b5b7a5a2d7af088760766a7dd50dea33
priydarshi12 <21001016054.jcboseust.ac.in>
2024-03-02 09:14:49 +0530

    Loading Card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,37 @@
-import React from 'react'
-
+import React, { useState, useEffect } from "react";
+import { getProductsByCount } from "../functons/product";
+import ProductCard from "../components/cards/ProductCard";
 const Home = () => {
-  return (
-    <div>
-      <p>react home</p>
-    </div>
-  )
-}
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-export default Home
+  useEffect(() => {
+    loadAllProduct();
+  }, []);
+  const loadAllProduct = () => {
+    setLoading(true);
+    getProductsByCount(3).then((res) => {
+      setProducts(res.data);
+      setLoading(false);
+    });
+  };
+  return (
+<>
+      <div className="jumbotron">
+        {loading ? <h4>Loading...</h4> : <h4>All Products</h4>}
+      </div>
+
+      <div className="container">
+        <div className="row">
+          {products.map((product) => (
+            <div key={product._id} className="col-md-4">
+              <ProductCard product={product} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
