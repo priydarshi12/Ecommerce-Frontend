35da883b8482fc7a14917d2492e41fd55d9b909f
priydarshi12 <21001016054.jcboseust.ac.in>
2024-03-04 17:08:00 +0530

    redirecting to intended page

--- src/components/cards/SingleProduct.js
+++ src/components/cards/SingleProduct.js
@@ -6,11 +6,12 @@ import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Laptop from "../../images/defaultImage.jpg";
 import ProductListItems from "./ProductListItems";
-
+import StarRating from "react-star-ratings";
+import RatingModal from "../modal/RatingModal";
 const { TabPane } = Tabs;
 
 const SingleProduct = ({ product }) => {
-  const { title, images, description } = product;
+  const { title, images, description, _id } = product;
 
   return (
     <>
@@ -35,6 +36,17 @@ const SingleProduct = ({ product }) => {
 
       <div className="col-md-5">
         <h1 className="bg-info p-3">{title}</h1>
+        <StarRating
+          name={_id}
+          numberOfStars={5}
+          rating={2}
+          changeRating={(newRating, name) =>
+            console.log("newRating", newRating, "name", name)
+          }
+          isSelectable={true}
+          starRatedColor="red"
+        />
+
         <Card
           actions={[
             <>
@@ -44,6 +56,18 @@ const SingleProduct = ({ product }) => {
             <Link to="/">
               <HeartOutlined className="text-info" /> <br /> Add to Wishlist
             </Link>,
+             <RatingModal>
+              <StarRating
+                name={_id}
+                numberOfStars={5}
+                rating={2}
+                changeRating={(newRating, name) =>
+                  console.log("newRating", newRating, "name", name)
+                }
+                isSelectable={true}
+                starRatedColor="red"
+              />
+            </RatingModal>,
           ]}
         >
           <ProductListItems product={product} />
--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,9 +1,9 @@
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import { auth, googleAuthProvider } from "../../firebase";
 import { signInWithPopup } from "firebase/auth";
 import { toast } from "react-toastify";
 import { Button } from "antd";
-import { GoogleOutlined ,UserAddOutlined} from "@ant-design/icons";
+import { GoogleOutlined, UserAddOutlined } from "@ant-design/icons";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 
@@ -23,8 +23,13 @@ const Login = ({ history }) => {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => ({ ...state }));
   // useEffect(() => {
-  //   if (user && user.token) history.push("/");
-  // }, [user]);
+  //   let intended = history.location.state;
+  //   if (intended) {
+  //     return;
+  //   } else {
+  //     if (user && user.token) history.push("/");
+  //   }
+  // }, [user,history]);
 
   const googleLogin = async () => {
     try {
@@ -37,66 +42,65 @@ const Login = ({ history }) => {
       //posting the id token to backend
       loginfn(idTokenResult.token)
         .then((res) => {
-          console.log("response from the backend on login",res);
-          if(res.status===201){
-          console.log("response from backend on login ",res);
-          const { displayName } = user.reloadUserInfo;
-          console.log("google result token->", displayName);
-          dispatch({
-            type: "LOGGED_IN_USER",
-            payload: {
-              email: user.email,
-              name: displayName,
-              token: idTokenResult.token,
-              role:res.data.role,
-              _id:res.data._id,
-              image:user.picture,
-            },
-          });
-          if(res.data.role==="admin"){
-            history.push("/admin/dashboard");
-          }else{
-            history.push("/user/history");
+          console.log("response from the backend on login", res);
+          if (res.status === 201) {
+            console.log("response from backend on login ", res);
+            const { displayName } = user.reloadUserInfo;
+            console.log("google result token->", displayName);
+            dispatch({
+              type: "LOGGED_IN_USER",
+              payload: {
+                email: user.email,
+                name: displayName,
+                token: idTokenResult.token,
+                role: res.data.role,
+                _id: res.data._id,
+                image: user.picture,
+              },
+            });
+            let intended = history.location.state;
+            if (intended) {
+              history.push(intended.from);
+            } else {
+              if (res.data.role === "admin") {
+                history.push("/admin/dashboard");
+              } else {
+                history.push("/user/history");
+              }
+            }
           }
-         
-        }
-      })
-        .catch((err) =>{
-          console.log("user not found->",err.message)
-          if(err.response && err.response.status === 401){
-          toast.error("Please register yourself")
-          dispatch({
-            type: "USER NOT FOUND",
-            payload: null,
-          });
+        })
+        .catch((err) => {
+          console.log("user not found->", err.message);
+          if (err.response && err.response.status === 401) {
+            toast.error("Please register yourself");
+            dispatch({
+              type: "USER NOT FOUND",
+              payload: null,
+            });
           }
         });
-
-    
     } catch (error) {
       console.log(error.message);
       toast.error(error.message);
     }
   };
 
-
   return (
     <div className="container p-5">
       <div className="row">
         <div className="col-md-6 offset-md-3">
-        
-            <Button
-              onClick={googleLogin}
-              type="danger"
-              className="mb-3"
-              block
-              shape="round"
-              icon={<GoogleOutlined />}
-              size="large"
-            >
-              Login with Google
-            </Button>
-          
+          <Button
+            onClick={googleLogin}
+            type="danger"
+            className="mb-3"
+            block
+            shape="round"
+            icon={<GoogleOutlined />}
+            size="large"
+          >
+            Login with Google
+          </Button>
         </div>
       </div>
     </div>
